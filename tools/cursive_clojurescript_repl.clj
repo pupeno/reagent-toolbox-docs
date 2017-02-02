f7c310fb147f2071cc39c27a67158a33818cba7e
J. Pablo Fernández <pupeno@pupeno.com>
2017-02-02 06:56:37 +0000

    Non-semantic: Cursive ClojureScript REPL.

---
+++ tools/cursive_clojurescript_repl.clj
@@ -0,0 +1,7 @@
+;;;; Copyright © 2017 Flexpoint Tech Ltd
+
+;;;; File to run a ClojureScript REPL in Cursive: https://github.com/bhauman/lein-figwheel/wiki/Running-figwheel-in-a-Cursive-Clojure-REPL
+
+(use 'figwheel-sidecar.repl-api)
+(start-figwheel!) ;; <-- fetches configuration
+(cljs-repl)
