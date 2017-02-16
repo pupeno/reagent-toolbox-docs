4ccf2e058c13b8c170539e7de582642b418b9d5c
J. Pablo Fernández <pupeno@pupeno.com>
2017-02-16 06:59:49 +0000

    Clean up the project file, moving many settings to the :dev profile.

--- project.clj
+++ project.clj
@@ -23,7 +23,8 @@
 
   :plugins [[lein-cljsbuild "1.1.4"]
             [lein-figwheel "0.5.9"]
-            [deraen/lein-sass4clj "0.3.0"]]
+            [deraen/lein-sass4clj "0.3.0"]
+            [lein-heroku "0.5.3"]]
 
   :min-lein-version "2.5.3"
 
@@ -33,29 +34,40 @@
                                     "resources/public/css"
                                     "target"]
 
-  :figwheel {:css-dirs       ["resources/public/css"]
-             :server-logfile "log/figwheel-logfile.log"
-             :ring-handler   reagent-toolbox-docs.core/app}
-
-  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]     ; Add "checkouts/reagent-toolbox/src/cljs" to load it on the fly.
-                             :figwheel     {:on-jsload "reagent-toolbox-docs.core/mount-root"}
-                             :compiler     {:main                 reagent-toolbox-docs.core
-                                            :output-to            "resources/public/js/app.js"
-                                            :output-dir           "resources/public/js"
-                                            :asset-path           "js/"
-                                            :source-map-timestamp true
-                                            :preloads             [devtools.preload]
-                                            :external-config      {:devtools/config {:features-to-install :all}}}}}}
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]     ; Add "../reagent-toolbox/src/cljs" to load it on the fly.
+                             :compiler     {:main       reagent-toolbox-docs.core
+                                            :output-to  "resources/public/js/app.js"
+                                            :output-dir "resources/public/js"
+                                            :asset-path "js/"}}}}
 
   :sass {:source-paths ["src/assets"]
          :target-path  "resources/public/css"
          :source-map   true}
 
-  :profiles {:dev  {:dependencies [[figwheel-sidecar "0.5.9"]
-                                   [binaryage/devtools "0.9.0"]
-                                   [com.cemerick/piggieback "0.2.1"]
-                                   [org.clojure/tools.nrepl "0.2.12"]]
-                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
-             :prod {:cljsbuild {:builds {:app {:optimizations   :advanced
-                                               :closure-defines {goog.DEBUG false}
-                                               :pretty-print    false}}}}})
+  :uberjar-name "reagent-toolbox-docs-standalone.jar"
+  :heroku {:app-name "reagent-toolbox-docs"}
+
+  :profiles {:dev     {:cljsbuild    {:builds {:app {:compiler {:preloads             [devtools.preload]
+                                                                :source-map-timestamp true
+                                                                :external-config      {:devtools/config {:features-to-install :all}}}
+                                                     :figwheel {:on-jsload "reagent-toolbox-docs.core/mount-root"}}}}
+                       :dependencies [[figwheel-sidecar "0.5.9"]
+                                      [binaryage/devtools "0.9.0"]
+                                      [com.cemerick/piggieback "0.2.1"]
+                                      [org.clojure/tools.nrepl "0.2.12"]]
+                       :figwheel {:css-dirs       ["resources/public/css"]
+                                  :server-logfile "log/figwheel-logfile.log"
+                                  :ring-handler   reagent-toolbox-docs.core/app}
+                       :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
+             :uberjar {:omit-source true
+                       :aot         :all
+                       :env         {:production "true"}
+                       :hooks       [leiningen.cljsbuild]
+                       :prep-tasks  [["cljsbuild" "once"]
+                                     "javac"
+                                     "compile"
+                                     #_["sass4clj" "once"]]
+                       :cljsbuild   {:jar    true
+                                     :builds {:app {:compiler {:optimizations   :advanced
+                                                               :closure-defines {goog.DEBUG false}
+                                                               :pretty-print    false}}}}}})
