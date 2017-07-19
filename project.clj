993b558aa96094b902d86e207ec5f4d18fae9ac0
J. Pablo Fernández <pupeno@pupeno.com>
2017-07-19 11:40:56 +0100

    Upgrade all the things and other figwheel changes.

--- project.clj
+++ project.clj
@@ -23,26 +23,28 @@
                                     "target"]
 
   :figwheel {:css-dirs       ["resources/public/css"]
-             :server-logfile "log/figwheel-logfile.log"
-             :ring-handler   reagent-toolbox-docs.core/app}
+             :server-logfile "log/figwheel.log"
+             :ring-handler   reagent-toolbox-docs.core/app
+             :server-port    3460}
 
   :uberjar-name "reagent-toolbox-docs-standalone.jar"
   :heroku {:app-name "reagent-toolbox-docs"}
 
   :shell {:commands {"lein" {:windows ["cmd.exe" "/c" "lein.bat"]}}}
 
-  :profiles {:dev     {:cljsbuild    {:builds {:app {:source-paths ["checkouts/reagent-toolbox/src/cljs"]
+  :profiles {:dev     {:cljsbuild    {:builds {:app {:source-paths ["..\\reagent-toolbox\\src\\cljs"]
                                                      :compiler     {:preloads             [devtools.preload]
                                                                     :source-map           true
                                                                     :source-map-timestamp true
                                                                     :pretty-print         true
                                                                     :external-config      {:devtools/config {:features-to-install :all}}}
                                                      :figwheel     {:on-jsload "reagent-toolbox-docs.core/mount-root"}}}}
-                       :dependencies [[figwheel-sidecar "0.5.9"]
-                                      [binaryage/devtools "0.9.0"]
-                                      [com.cemerick/piggieback "0.2.1"]
-                                      [org.clojure/tools.nrepl "0.2.12"]
-                                      [org.slf4j/slf4j-nop "1.7.13"]]
+                       :dependencies [[figwheel "0.5.11"]
+                                      [figwheel-sidecar "0.5.11"]
+                                      [binaryage/devtools "0.9.4"]
+                                      [com.cemerick/piggieback "0.2.2"]
+                                      [org.clojure/tools.nrepl "0.2.13"]
+                                      [org.slf4j/slf4j-nop "1.7.25"]]
                        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
              :uberjar {:omit-source true
                        :aot         :all
@@ -51,7 +53,7 @@
                        :prep-tasks  ["javac"
                                      "compile"
                                      ["cljsbuild" "once"]
-                                     #_["sass4clj" "once"]  ; This should be the correct way of compiling SCSS, but: https://github.com/Deraen/sass4clj/issues/18
+                                     #_["sass4clj" "once"]                                                              ; This should be the correct way of compiling SCSS, but: https://github.com/Deraen/sass4clj/issues/18
                                      ["shell" "lein" "sass4clj" "once"]]
                        :cljsbuild   {:jar    true
                                      :builds {:app {:compiler {:closure-defines {goog.DEBUG false}}}}}}}
@@ -59,27 +61,26 @@
   :min-lein-version "2.5.3"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.456"]
-                 [cljsjs/react-with-addons "15.4.2-2"]
-                 [reagent "0.6.1" :exclusions [cljsjs/react]]
-                 [re-frame "0.9.2"]
+                 [org.clojure/clojurescript "1.9.671"]
+                 [cljsjs/react-with-addons "15.6.1-0"]
+                 [reagent "0.7.0" :exclusions [cljsjs/react]]
+                 [re-frame "0.9.4"]
                  [com.domkm/silk "0.1.2"]
-                 [kibu/pushy "0.3.6"]
-                 [compojure "1.5.2"]
-                 [ring/ring-defaults "0.2.3"]
-                 [ring/ring-jetty-adapter "1.5.1"]
+                 [kibu/pushy "0.3.7"]
+                 [compojure "1.6.0"]
+                 [ring/ring-defaults "0.3.0"]
+                 [ring/ring-jetty-adapter "1.6.2"]
                  [environ "1.1.0"]
                  [replumb "0.2.4"]
-                 [org.clojure/tools.reader "1.0.0-beta1"]   ; Required by replumb
-                 [com.cognitect/transit-clj "0.8.297"]      ; Required by replumb
-                 [com.cognitect/transit-cljs "0.8.239"]     ; Required by replumb
-                 [cljsjs/codemirror "5.21.0-2"]
+                 [org.clojure/tools.reader "1.0.2"]                                                                     ; Required by replumb
+                 [com.cognitect/transit-clj "0.8.300"]                                                                  ; Required by replumb
+                 [com.cognitect/transit-cljs "0.8.239"]                                                                 ; Required by replumb
+                 [cljsjs/codemirror "5.24.0-1"]
                  [camel-snake-kebab "0.4.0"]
-                 [cheshire "5.7.0"]                         ; Required by sass4clj
+                 [cheshire "5.7.1"]                                                                                     ; Required by sass4clj
                  [tech.dashman/reagent-toolbox "0.1.0-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.1.4"]
-            ;[lein-figwheel "0.5.9"]
             [deraen/lein-sass4clj "0.3.0"]
             [lein-heroku "0.5.3"]
             [lein-shell "0.5.0"]])
